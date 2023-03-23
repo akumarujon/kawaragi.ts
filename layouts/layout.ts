97b4dd5f1f091359245e56a1c3693c0f1401d6db
user6879 <akumaru.senju@gmail.com>
2023-03-23 21:41:59 +0500

    working YIIAASS

--- layouts/layout.ts
+++ layouts/layout.ts
@@ -6,9 +6,7 @@ export default function (title: string, style: string, body: string): string {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
-    <style>
-        ${style}
-    <style/>
+    <style>${style}</style>
 </head>
 <body>
  ${body}
--- routes/post.ts
+++ routes/post.ts
@@ -2,7 +2,6 @@ import { app } from "../app/mod.ts";
 import { MarkDownToHTML } from "../utils/mod.ts";
 
 app.get("/:post", async (req, res) => {
-  console.log(await MarkDownToHTML(req.params.post))
   await res.send(await MarkDownToHTML(req.params.post)).end();
 });
 
