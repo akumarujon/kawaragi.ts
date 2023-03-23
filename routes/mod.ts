9a6f19a3424f72081e4567a91e8060609f73110d
user6879 <akumaru.senju@gmail.com>
2023-03-23 18:57:28 +0500

    [ADD], rr

--- routes/mod.ts
+++ routes/mod.ts
@@ -1,3 +1,4 @@
+import "./rr.ts";
 import "./home.ts";
 import "./source.ts";
 import "./post.ts";
---
+++ routes/rr.ts
@@ -0,0 +1,5 @@
+import { app } from "../app/mod.ts";
+
+import { rr } from "https://gist.githubusercontent.com/triistam/296b8c2978f8869edc4bd53c4ad9687c/raw/4b59e841c8941873b11a42733bb9a9108731543d/rr.ts"
+app.get('/rr', rr)
+
