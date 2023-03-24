b3d0afac5fc94a41074e84e8e393575d822c1f74
user6879 <akumaru.senju@gmail.com>
2023-03-24 19:16:10 +0500

    fix: title

--- utils/converter.ts
+++ utils/converter.ts
@@ -4,8 +4,8 @@ import to_html from "../layouts/layout.ts";
 
 export async function MarkDownToHTML(filename: string): Promise<string> {
   const css_path: string = await Deno.readTextFile(Deno.cwd() + "/static/style.css");
-  const css_link: string = " 	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css";
-  const title:string = "Post"
+  const css_link: string = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css";
+  let title:string = filename.replace('-', ' ');
   const markdown = await Deno.readTextFile(
     Deno.cwd() + `/posts/${filename}.md`,
   );
@@ -17,6 +17,7 @@ export async function MarkDownToHTML(filename: string): Promise<string> {
     tables: true,
   });
 
+  title = title.charAt(0).toUpperCase() + title.slice(1);
   await showdown.setFlavor("github");
   return to_html(title, css_link,css_path, converter.makeHtml(markdown))
 }
