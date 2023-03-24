e5fc016d1a88ba4c03e649c1c7cec311701bab65
user6879 <akumaru.senju@gmail.com>
2023-03-24 19:16:20 +0500

    show available posts

---
+++ utils/posts.ts
@@ -0,0 +1,14 @@
+export default async function(): Promise<string> {
+    let files:string[] = []
+
+    for await(const element of Deno.readDir(Deno.cwd() + '/posts')){
+        let filename:string = element.name.split('.md')[0];
+        let title:string = filename.replace('-', ' ');
+        await files.push(`<li><a href="/${filename}" class="link">${title}</a></li>`)
+    }
+
+    return `<ul>
+${files.join('\n')}    
+</ul>`
+}
+
