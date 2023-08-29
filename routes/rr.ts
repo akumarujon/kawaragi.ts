5d95396d51b76dd09fd1d3237d1729f764a89627
akumarujon <124185383+akumarujon@users.noreply.github.com>
2023-08-29 10:14:55 +0500

    new link

--- routes/rr.ts
+++ routes/rr.ts
@@ -1,4 +1,4 @@
 import { app } from "../app/mod.ts";
 
-import { rr } from "https://gist.githubusercontent.com/triistam/296b8c2978f8869edc4bd53c4ad9687c/raw/4b59e841c8941873b11a42733bb9a9108731543d/rr.ts";
+import { rr } from "https://gist.githubusercontent.com/akumarujon/296b8c2978f8869edc4bd53c4ad9687c/raw/9e9e64cc2af9f9cea2ca4864406252eb0b950b6c/rr.ts";
 app.get("/rr", rr);
